56c068a44216c838bdf2b74236f5f5c6b21ce6f6
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-01-17 09:17:44 +0100

    Installation and setup of font-awesome.

--- src/main.js
+++ src/main.js
@@ -5,4 +5,19 @@ import router from "./router";
 import store from "./store";
 import "./main.scss";
 
-createApp(App).use(store).use(router).mount("#app");
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faUser);
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
